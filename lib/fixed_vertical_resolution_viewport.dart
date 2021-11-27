a6038284ea009b71c00320f9c51538c76b0972ee
Christian <christian.muehle@devowl.de>
2021-11-27 19:09:18 +0100

    Simplified viewport

--- lib/fixed_vertical_resolution_viewport.dart
+++ lib/fixed_vertical_resolution_viewport.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flame/extensions.dart';
 import 'package:flame/game.dart';
 
@@ -9,7 +11,7 @@ class FixedVerticalResolutionViewport extends Viewport {
   /// The effective height of the viewport.
   final double effectiveHeight;
 
-  final Vector2 _scaledSize = Vector2.zero();
+  var _scaledSize = Vector2.zero();
 
   /// The scaled size of this viewport.
   Vector2 get scaledSize => _scaledSize.clone();
@@ -27,9 +29,6 @@ class FixedVerticalResolutionViewport extends Viewport {
   /// The matrix used for scaling and translating the canvas
   final Matrix4 _transform = Matrix4.identity();
 
-  /// The Rect that is used to clip the canvas
-  late Rect _clipRect;
-
   @override
   void resize(Vector2 newCanvasSize) {
     canvasSize = newCanvasSize.clone();
@@ -37,14 +36,16 @@ class FixedVerticalResolutionViewport extends Viewport {
     _scale = canvasSize!.y / effectiveHeight;
 
     _scaledSize
-      ..setFrom(effectiveSize)
-      ..scale(_scale);
+      ..setFrom(Vector2(
+        canvasSize!.x,
+        effectiveHeight,
+      ))
+      ..multiply(Vector2(1, _scale));
+
     _resizeOffset
       ..setFrom(canvasSize!)
       ..sub(_scaledSize)
       ..scale(0.5);
-
-    _clipRect = _resizeOffset & _scaledSize;
 
     _transform
       ..setIdentity()
@@ -56,7 +57,6 @@ class FixedVerticalResolutionViewport extends Viewport {
   void render(Canvas c, void Function(Canvas) renderGame) {
     c
       ..save()
-      ..clipRect(_clipRect)
       ..transform(_transform.storage);
     renderGame(c);
     c.restore();
