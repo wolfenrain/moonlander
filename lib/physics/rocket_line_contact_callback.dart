5b2c70f22d6bb444c9a238907afa016b3a57a4cc
Christian <christian.muehle@devowl.de>
2022-02-13 16:43:56 +0100

    Impact handling for line and rocket

--- lib/physics/rocket_line_contact_callback.dart
+++ lib/physics/rocket_line_contact_callback.dart
@@ -1,22 +1,49 @@
 import 'package:flame_forge2d/contact_callbacks.dart';
 import 'package:flame_forge2d/flame_forge2d.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:moonlander/components/line_component.dart';
 import 'package:moonlander/components/rocket_component.dart';
+import 'package:moonlander/game_state.dart';
 
 /// Handles collision between rocket and line.
 class RocketLineContactCallback
     extends ContactCallback<RocketComponent, LineComponent> {
+  double _totalImpactImpulse = 0;
+  int _contactCounter = 1;
+
   @override
-  void begin(RocketComponent a, LineComponent b, Contact contact) {}
+  void begin(RocketComponent a, LineComponent b, Contact contact) {
+    _contactCounter = 1;
+    _totalImpactImpulse = 0;
+  }
+
+  @override
+  void postSolve(
+    RocketComponent a,
+    LineComponent b,
+    Contact contact,
+    ContactImpulse impulse,
+  ) {
+    if (contact.isTouching()) {
+      // The impact impules of the two bodies.
+      _totalImpactImpulse += impulse.normalImpulses[0].abs();
+      _contactCounter++;
+    }
+  }
 
   @override
   void end(RocketComponent a, LineComponent b, Contact contact) {
-    var crashed = b.isGoal;
-
-    if (crashed) {
-      a.loose();
+    if (GameState.playState == PlayingState.playing) {
+      final avgImpactImpulse = _totalImpactImpulse / _contactCounter;
+      final crashed = avgImpactImpulse > 15;
+      debugPrint('impact impulse total: $avgImpactImpulse');
+      if (!crashed && b.isGoal) {
+        a.win(b, avgImpactImpulse);
+      } else if (crashed) {
+        a.loose();
+      }
     } else {
-      a.win(b);
+      debugPrint('Contact while not playing');
     }
   }
 }
